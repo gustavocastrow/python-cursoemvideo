d213da32e1b5a5abc2bdcf5174bd7befa1646d4a
Gustavo Castro <gustavocastrocs@gmail.com>
2020-10-17 10:36:34 -0300

    Manipulando Texto

---
+++ mundo01/aula009.py
@@ -0,0 +1,30 @@
+#Manipulando texto
+
+#Fatiamento de string
+#Análise de string:
+#len(frase) -> comprimento da frase
+#frase.count('letra') -> conta quantas vezes a letra entre ' ' aparece na string
+#frase.count('letra', 0,13) -> contagem com fatiamento
+#frase.find('letras') -> quantas vezes encontrou as letras entre ' '
+#'Curso' in frase -> Existe a palavra curso dentro da string frase, retorna true or false
+
+#Transformação
+#frase.replace('python', 'javascript')  -> onde tiver python vai substituir por android
+#frase.upper() -> transforma a string em maisculas
+#frase.lower() -> transfroma a string em minusculas
+#frase.capitalize() -> joga uma string inteira para minusculo e transforma a primeira letra em maisculo
+#frase.title() -> analise quantas palavras tem a string, e transforma a primeira letra em maiscula
+#frase.strip() -> remove os espaços sem utilidades nas strings
+#frase.rstrip() -> remove os espaços da direita(R), no caso no final da string
+#frase.lstrip() -> remove os espaços da esquerda(L), no caso no inicio da frase
+
+#Divisão de Strings
+
+
+#frase.split() -> cada palavra recebe index novo, cada palavra vira uma nova lista
+#'-'.join(frase) -> juntar todas os elementos da frase e vai usar o separador '-'
+
+
+
+
+
